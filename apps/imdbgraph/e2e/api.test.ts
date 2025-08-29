b3c3d293893d19cc05ead9f7d422f3550e6ed491
Aria <amini5454@gmail.com>
2025-08-29 18:49:42 -0400

    Remove test

--- apps/imdbgraph/e2e/api.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test'
-import searchData from '../__mocks__/hars/data.json' with { type: 'json' }
-
-test('search api should work', async ({ request }) => {
-	const resp = await request.get('/api/suggestions?q=Avatar')
-	expect(await resp.json()).toEqual(searchData)
-})
